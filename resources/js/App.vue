446b3cd322c681994a3250ee532271099d65fc8f
Grevcev <tas_devil@rambler.ru>
2021-06-22 22:10:34 +0300

    Изменение файлов для vue

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h2 style="text-align: center">Общая страница</h2>
+        <p style="text-align: center">
+            Тест общей страницы
+        </p>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
